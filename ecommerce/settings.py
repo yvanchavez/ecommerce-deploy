215048b4a2cf1a29df9b1f5c7b5e807cfe968504
yvan <chavez.yvan@gmail.com>
2020-08-23 21:14:07 -0500

    deploy pythonanywhere

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1^yf(p1kskf1#03yn0n&5^mn&(cn8&bns67x+u5b_q-@#_3=5e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
